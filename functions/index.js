caed4b1bb081e55a6615c4dbd1f4e5c2b5544588
Daniel Reinoso <danielreinoso1807@gmail.com>
2020-05-28 08:22:20 -0400

    Test CORS fix

--- functions/index.js
+++ functions/index.js
@@ -7,18 +7,21 @@ const ml = google.ml("v1");
 exports.getPrediction = functions
   .region("europe-west1")
   .https.onRequest(async (req, res) => {
-    const adc = await google.auth.getApplicationDefault();
-
-    res.set("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type');
 
     //respond to CORS preflight requests
     if (req.method === "OPTIONS") {
-      res.set("Access-Control-Allow-Methods", "POST");
-      res.set("Access-Control-Allow-Headers", "Content-Type");
-      res.set("Access-Control-Max-Age", "3600");
       res.status(204).send("");
       return;
     }
+  
+    res.header("Content-Type", "application/json");
+  
+    const adc = await google.auth.getApplicationDefault();
+
     // create the full model name which includes the project ID
     const model = "BookReviewsSentiment";
     const modelName = "projects/" + adc.projectId + "/models/" + model;
