b32c5947a59733501c4f3c8cee95b50dfe110215
Daniele Cordano <daniele.cordano@gmail.com>
2020-05-28 14:17:16 +0200

    Removed unnecessary scripts

--- functions/index_express.js
+++ functions/index_express.js
@@ -1,19 +1,23 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors({ origin: true }));
 admin.initializeApp();
 const { google } = require("googleapis");
 const ml = google.ml("v1");
 
-exports.getPrediction = functions
-  .region("europe-west1")
-  .https.onRequest(async (req, res) => {
+app.post("/getPrediction", (req, res) => {
+  (async () => {
     const adc = await google.auth.getApplicationDefault();
-
+    // Set CORS headers for preflight requests
+    // Allows GETs from any origin with the Content-Type header
+    // and caches preflight response for 3600s
     res.set("Access-Control-Allow-Origin", "*");
-
-    //respond to CORS preflight requests
     if (req.method === "OPTIONS") {
-      res.set("Access-Control-Allow-Methods", "POST");
+      // Send response to OPTIONS requests
+      res.set("Access-Control-Allow-Methods", "GET");
       res.set("Access-Control-Allow-Headers", "Content-Type");
       res.set("Access-Control-Max-Age", "3600");
       res.status(204).send("");
@@ -37,4 +41,7 @@ exports.getPrediction = functions
         res.status(200).send(result.data["predictions"]);
       }
     });
-  });
+  })();
+});
+
+exports.app = functions.region("europe-west1").https.onRequest(app);
